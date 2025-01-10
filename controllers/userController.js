027eba79cad6d04a06bd90215dff41c96ff4db85
Alberto C. Garcia <albertogarcia767@yahoo.com>
2025-01-09 21:48:58 -0500

    updated userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,13 +4,15 @@ module.exports = {
   // Get all users
   async getUsers(req, res) {
     try {
-      const users = await User.find();  // Simplified query first
+      // Simplified query without populate
+      const users = await User.find();
       
-      if (!users) {
+      console.log('Retrieved users:', users); // Debug log
+      
+      if (!users || users.length === 0) {
         return res.status(404).json({ message: 'No users found' });
       }
 
-      console.log('Retrieved users:', users); // Debug log
       res.json(users);
     } catch (err) {
       console.log('Error in getUsers:', err); // Debug log
@@ -20,32 +22,28 @@ module.exports = {
       });
     }
   },
+  // Get single user
   async getSingleUser(req, res) {
     try {
-      const user = await User.findOne({ _id: req.params.userId })
-        .select('-__v')
-        .populate("thoughts")
-        .populate("friends");
-
+      const user = await User.findOne({ _id: req.params.userId });
       if (!user) {
         return res.status(404).json({ message: 'No user with that ID' });
       }
-
       res.json(user);
     } catch (err) {
       res.status(500).json(err);
     }
   },
-  // create a new user
+  // Create user
   async createUser(req, res) {
     try {
-      const dbUserData = await User.create(req.body);
-      res.json(dbUserData);
+      const user = await User.create(req.body);
+      res.json(user);
     } catch (err) {
       res.status(500).json(err);
     }
   },
-
+  // Update user
   async updateUser(req, res) {
     try {
       const user = await User.findOneAndUpdate(
@@ -53,77 +51,27 @@ module.exports = {
         { $set: req.body },
         { runValidators: true, new: true }
       );
-
       if (!user) {
         return res.status(404).json({ message: 'No user with this id!' });
       }
-
       res.json(user);
     } catch (err) {
-      console.log(err);
       res.status(500).json(err);
     }
   },
+  // Delete user
   async deleteUser(req, res) {
     try {
-      console.log('Attempting to delete user with ID:', req.params.userId);
       const user = await User.findOneAndDelete({ _id: req.params.userId });
-
-      if (!user) {
-        return res.status(404).json({ 
-          message: 'No user with this id!', 
-          requestedId: req.params.userId 
-        });
-      }
-
-      res.json({ 
-        message: 'User successfully deleted!',
-        deletedUser: user 
-      });
-    } catch (err) {
-      console.log('Error deleting user:', err);
-      res.status(500).json({ 
-        message: 'Error deleting user',
-        error: err.message 
-      });
-    }
-  },
-  // Add a user reaction
-  async addReaction(req, res) {
-    try {
-      const user = await User.findOneAndUpdate(
-        { _id: req.params.userId },
-        { $addToSet: { reactions: req.body } },
-        { runValidators: true, new: true }
-      );
-
       if (!user) {
         return res.status(404).json({ message: 'No user with this id!' });
       }
-
-      res.json(user);
+      res.json({ message: 'User deleted!' });
     } catch (err) {
       res.status(500).json(err);
     }
   },
-  // Remove user reaction
-  async removeReaction(req, res) {
-    try {
-      const user = await User.findOneAndUpdate(
-        { _id: req.params.userId },
-        { $pull: { reactions: { reactionId: req.params.reactionId } } },
-        { runValidators: true, new: true }
-      );
-
-      if (!user) {
-        return res.status(404).json({ message: 'No user with this id!' });
-      }
-
-      res.json(user);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  },
+  // Add friend
   async addFriend(req, res) {
     try {
       const user = await User.findOneAndUpdate(
@@ -139,6 +87,7 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+  // Remove friend
   async removeFriend(req, res) {
     try {
       const user = await User.findOneAndUpdate(
@@ -153,5 +102,5 @@ module.exports = {
     } catch (err) {
       res.status(500).json(err);
     }
-  },
+  }
 };
