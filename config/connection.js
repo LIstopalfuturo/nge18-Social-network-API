884cab80b27547927e5243b3e156cd7a3ade57ce
Alberto C. Garcia <albertogarcia767@yahoo.com>
2025-01-10 11:03:24 -0500

    changes

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
-// Connect to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/socialNetworkDB');
+mongoose.connect('mongodb://127.0.0.1:27017/socialNetworkDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 
